7869a62b8e4a0a0c4c63695b2e2134cd82ea811e
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2024-05-18 11:13:00 +0700

    style: change some labels

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -1,6 +1,10 @@
 <?php
 
+use Filament\Actions\Action;
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Auth\Login as BaseAuth;
+use Illuminate\Contracts\Support\Htmlable;
 
 class Login extends BaseAuth
 {
@@ -16,5 +20,28 @@ class Login extends BaseAuth
           ->statePath('data'),
       ),
     ];
+  }
+
+  protected function getEmailFormComponent(): Component
+  {
+    return TextInput::make('email')
+      ->label('Email')
+      ->email()
+      ->required()
+      ->autocomplete()
+      ->autofocus()
+      ->extraInputAttributes(['tabindex' => 1]);
+  }
+
+  public function getHeading(): string | Htmlable
+  {
+    return 'Masuk';
+  }
+
+  protected function getAuthenticateFormAction(): Action
+  {
+    return Action::make('authenticate')
+      ->label('Masuk')
+      ->submit('authenticate');
   }
 }
