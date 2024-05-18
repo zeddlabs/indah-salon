7bc07b961c9df881553c5cf8db73ebe159053913
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2024-05-18 11:11:30 +0700

    fix: implement FilamentUser

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,26 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasName
 {
     use HasFactory;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
+    public function getFilamentName(): string
+    {
+        return $this->nama;
+    }
 
     /**
      * The attributes that are mass assignable.
